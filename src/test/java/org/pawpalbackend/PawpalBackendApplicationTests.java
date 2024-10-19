a7b00c5673a667d89279bd6727ac3f3c58eeb49c
Denis <93794328+denis-vp@users.noreply.github.com>
2024-10-19 15:59:18 +0300

    initial commit

---
+++ src/test/java/org/pawpalbackend/PawpalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pawpalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
