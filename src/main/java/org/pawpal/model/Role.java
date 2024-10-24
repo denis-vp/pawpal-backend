abc940b65544f24d4a586d57edbb2643122822ad
mario17s <monu.mario@yahoo.com>
2024-10-24 11:44:38 +0300

    add getters and setters

--- src/main/java/org/pawpal/model/Role.java
+++ src/main/java/org/pawpal/model/Role.java
@@ -18,4 +18,20 @@ public class Role {
     }
 
     public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/org/pawpal/model/User.java
+++ src/main/java/org/pawpal/model/User.java
@@ -48,4 +48,68 @@ public class User {
         this.isNew = isNew;
         this.roles = roles;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPasswordAttempts() {
+        return passwordAttempts;
+    }
+
+    public void setPasswordAttempts(int passwordAttempts) {
+        this.passwordAttempts = passwordAttempts;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
